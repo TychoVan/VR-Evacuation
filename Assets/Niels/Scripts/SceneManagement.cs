fe22e8f7949fac677cd559e530b5c540ba5ade32
NnelisS <78425965+NnelisS@users.noreply.github.com>
2022-10-07 09:01:55 +0200

    Updates

---
+++ Assets/Niels/Scripts/SceneManagement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Layout");
+    }
+
+    public void Win()
+    {
+        SceneManager.LoadScene("Win");
+    }
+
+    public void Lose()
+    {
+        SceneManager.LoadScene("Lose");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            MainMenu();
+    }
+}
